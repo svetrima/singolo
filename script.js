b9b155e3d11e22a2892fce7e2d21912764ed64bf
Svetrima <svetsmetanina@gmail.com>
2020-03-15 18:10:06 +0300

    fix: markup, js, rules

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menuLinks = document.querySelector('.navigation');
 const portfolioTags = document.querySelector('.portfolio__tags');
-const portfolioGalleryBlock = document.querySelector('.gallery-block');
+const portfolioGalleryBlock = document.querySelector('.layout-4-columns');
 
 //navigation
 menuLinks.addEventListener('click', (event) => {
@@ -13,4 +13,9 @@ portfolioTags.addEventListener('click', (event) => {
     portfolioTags.querySelectorAll('span').forEach(el => el.classList.remove('tag_selected'));
     event.target.classList.add('tag_selected');
 
+})
+//portfolio gallery
+portfolioGalleryBlock.addEventListener('click', (event) => {
+    portfolioGalleryBlock.querySelectorAll('.gallery-block__item').forEach(el => el.classList.remove('outline'));
+    event.target.classList.add('outline');
 })
