0424910700c6fa0bcde67314f3e9bb4c25a3d0f5
Svetrima <svetsmetanina@gmail.com>
2020-03-15 16:21:21 +0300

    feat:add code js

---
+++ script.js
@@ -0,0 +1,16 @@
+const menuLinks = document.querySelector('navigation');
+const portfolioTags = document.querySelector('.portfolio__tags');
+const portfolioGalleryBlock = document.querySelector('.gallery-block');
+
+//navigation
+menuLinks.addEventListener('click', (event) => {
+        menuLinks.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+        event.target.classList.add('active');
+    })
+
+//portfolio tags
+portfolioTags.addEventListener('click', (event) => {
+    portfolioTags.querySelectorAll('span').forEach(el => el.classList.remove('tag_selected'));
+    event.target.classList.add('tag_selected');
+
+})
